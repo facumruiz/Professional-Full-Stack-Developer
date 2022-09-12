17f5971e5e9dd7d804e6db879ee8eceb64e9dc9f
facumruiz <facundoruiz105@gmail.com>
2022-09-12 15:02:46 -0300

    agrego syntax error a division

--- Modulo1/TP2(optativo)/script.js
+++ Modulo1/TP2(optativo)/script.js
@@ -1,52 +1,56 @@
 // Fucion Array para operaciones
-const sumar = (a,b) =>{
-    return  a+b;
-}
-const restar = (a,b) =>{
-    return  a-b;
-}
-const multiplicar = (a,b) =>{
-    return  a*b;
-}
-const dividir = (a,b) =>{
-    return  a/b;
-}
+const sumar = (a, b) => {
+  return a + b;
+};
+const restar = (a, b) => {
+  return a - b;
+};
+const multiplicar = (a, b) => {
+  return a * b;
+};
+const dividir = (a, b) => {
+  return a / b;
+};
 
 //Funcion por BOTONES
-function suma(){
+function suma() {
+  let n1 = parseInt(document.getElementById("N1").value);
+  let n2 = parseInt(document.getElementById("N2").value);
+  console.log(n1, n2);
 
-    let n1 = parseInt( document.getElementById("N1").value)
-    let n2 = parseInt( document.getElementById("N2").value)
-
-    let operacion_suma = (sumar(n1,n2))
-    document.getElementById("resultado").innerHTML = `${operacion_suma}`
-
+  let operacion_suma = sumar(n1, n2);
+  document.getElementById("resultado").innerHTML = `${operacion_suma}`;
 }
 
-function resta(){
-    let n1 = document.getElementById("N1").value
-    let n2 = document.getElementById("N2").value    
+function resta() {
+  let n1 = parseInt(document.getElementById("N1").value);
+  let n2 = parseInt(document.getElementById("N2").value);
+  console.log(n1, n2);
 
-    let operacion_resta = (restar(n1,n2))
-    document.getElementById("resultado").innerHTML = `${operacion_resta}`
+  let operacion_resta = restar(n1, n2);
+  document.getElementById("resultado").innerHTML = `${operacion_resta}`;
 }
 
-function multi(){
-    var n1 = document.getElementById("N1").value
-    var n2 = document.getElementById("N2").value
+function multi() {
+  let n1 = parseInt(document.getElementById("N1").value);
+  let n2 = parseInt(document.getElementById("N2").value);
+  console.log(n1, n2);
 
-    var operacion_multi = (multiplicar(n1,n2))
-    document.getElementById("resultado").innerHTML = `${operacion_multi}`
+  let operacion_multi = multiplicar(n1, n2);
+  document.getElementById("resultado").innerHTML = `${operacion_multi}`;
 }
 
+function divi() {
+  let n1 = parseInt(document.getElementById("N1").value);
+  let n2 = parseInt(document.getElementById("N2").value);
 
-function divi(){
-    let n1 = document.getElementById("N1").value
-    let n2 = document.getElementById("N2").value    
+  if (n2 === 0) {
+    document.getElementById(
+      "resultado"
+    ).innerHTML = `no se puede dividir entre 0`;
+  } else {
+    let operacion_dividir = dividir(n1, n2);
 
-    let operacion_dividir = (dividir(n1,n2))
-    document.getElementById("resultado").innerHTML = `${operacion_dividir}`
-
+    document.getElementById("resultado").innerHTML = `${operacion_dividir}`;
+  }
 }
-
-
